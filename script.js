cd66c9d4366a087669ed0d92c6079dbf5ad996a4
Aidan Zimmer <aidan.zimmer@sap.com>
2023-05-12 14:46:24 +0200

    Aufgabe "fertig"

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 console.log("hello world")
 
+let form;
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loaded")
-})
 
-function onButtonPress(e) {
-    alert("Knopf gedrückt")
-}
+    form = document.getElementsByTagName('form')[0]
+    console.log(form)
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        let hobbyType = document.getElementById('hobbyAuswahl').value;
+        let inputValue = document.getElementById('neuesHobby').value;
+        let list;
+        switch (hobbyType) {
+            case "Sport":
+                list = document.getElementById('Sport');
+                break;
+            case "Alle Hobbies":
+                list = document.getElementById('Alle Hobbies')
+                break;
+        }
+
+        var li = document.createElement("li");
+        li.innerText = inputValue
+        list.appendChild(li);
+        document.getElementById('neuesHobby').value = "";
+    })
+})
